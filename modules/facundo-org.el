28665665b8b28c7c1eed68cda47e61ebe95c9005
Facundo Olano <facundo.olano@gmail.com>
2018-03-17 13:34:13 -0300

    added org-present

--- modules/facundo-org.el
+++ modules/facundo-org.el
@@ -8,9 +8,11 @@
 
 ;;; Code:
 
-;;; TAKEN FROM prelude-org.el
+(prelude-require-package 'org)
 
-(add-to-list 'auto-mode-alist '("\\.org\\’" . org-mode))
+(require 'org)
+(require 'org-present)
+
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
@@ -27,8 +29,34 @@
 
 (add-hook 'org-mode-hook 'prelude-org-mode-defaults)
 
-;;; CUSTOM STUFF
 (setq org-startup-folded nil)
+
+;; syntax highlight code snippets
+(setq org-src-fontify-natively t)
+
+;; setup presentation mode
+(eval-after-load "org-present"
+  '(progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (toggle-frame-fullscreen)
+                 (org-present-big)
+                 (org-display-inline-images)
+                 ;; (org-present-hide-cursor)
+                 (flyspell-mode -1)
+                 (org-present-read-only)))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (toggle-frame-fullscreen)
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 ;; (org-present-show-cursor)
+                 (flyspell-mode +1)
+                 (org-present-read-write)))))
+
+(define-key org-mode-map (kbd "<f5>") 'org-present)
+(define-key org-present-mode-keymap (kbd "q") 'org-present-quit)
+(define-key org-present-mode-keymap (kbd "<f5>") 'org-present-quit)
 
 (provide 'facundo-org)
 
