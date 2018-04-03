299d0964355e9556756b5e20f9519b563d90ef9b
Facundo Olano <facundo.olano@gmail.com>
2018-04-03 11:40:34 -0300

    translation key bindings

--- modules/facundo-dictionary.el
+++ modules/facundo-dictionary.el
@@ -1,8 +1,15 @@
 (prelude-require-package 'google-translate)
 
+(require 'google-translate)
 (require 'google-translate-smooth-ui)
 
 (setq google-translate-translation-directions-alist
       '(("en" . "es") ("es" . "en") ))
+
+(setq google-translate-default-source-language "en")
+(setq google-translate-default-target-language "es")
+
+(global-set-key (kbd "s-t") 'google-translate-at-point)
+(global-set-key (kbd "s-T") 'google-translate-at-point-reverse)
 
 (provide 'facundo-dictionary)
