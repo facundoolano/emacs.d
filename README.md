aec3fd093dbf5e498e4acae8f6f6494bcae60891
Facundo Olano <facundo.olano@gmail.com>
2019-02-17 14:50:24 -0300

    added racer for rust autocompletion

--- README.md
+++ README.md
@@ -2,12 +2,19 @@ My emacs configuration. Previously based on prelude.
 
 ``` shell
 brew install emacs --with-cocoa --with-gnutls --with-dbus --with-librsvg --with-imagemagick@6 --with-mailutils --devel
+
 npm install -g livedown
 npm install -g mocha
 npm install -g eslint
+
 brew install candid82/brew/joker
+
 brew install aspell
+
 rustup component add rustfmt
+rustup toolchain add nightly
+rustup component add rust-src
+cargo install racer
 ```
 
 run from spotlight:
--- modules/facundo-rust.el
+++ modules/facundo-rust.el
@@ -1,6 +1,20 @@
 (require 'facundo-programming)
-(prelude-require-packages '(rust-mode))
+(prelude-require-packages '(rust-mode racer))
 
 (setq rust-format-on-save t)
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+
+(customize-set-variable 'racer-complete-insert-argument-placeholders nil)
+
+(defun racer-describe-and-switch ()
+  "`racer-describe` symbol at point and switch to the temp buffer for easier killing."
+  (interactive)
+  (racer-describe)
+  (other-window 1))
+
+(define-key rust-mode-map (kbd "M-?") 'racer-describe-and-switch)
 
 (provide 'facundo-rust)
