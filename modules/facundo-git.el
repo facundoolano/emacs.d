d308b49d7a96c8b79fafdc4a313df06040e4cfd0
Facundo Olano <facundo.olano@gmail.com>
2020-12-28 21:56:13 -0300

    better git browse package

--- modules/facundo-git.el
+++ modules/facundo-git.el
@@ -4,7 +4,7 @@
                             gitconfig-mode
                             gitignore-mode
                             github-review
-                            github-browse-file))
+                            git-link))
 
 (require 'magit)
 
@@ -12,22 +12,22 @@
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 (define-key magit-hunk-section-map (kbd "S-<return>") 'magit-diff-visit-file-other-window)
 
-;; these are not working for some reason, prefix not allowed
-;; (global-set-key (kbd "s-m m") 'magit-status)
-;; (global-set-key (kbd "s-m l") 'magit-log)
-;; (global-set-key (kbd "s-m f") 'magit-log-buffer-file)
-;; (global-set-key (kbd "s-m b") 'magit-blame)
-
 (add-to-list 'magit-dwim-selection '(magit-branch-and-checkout nil t))
 
 (add-to-list 'magit-section-initial-visibility-alist '(stashes . hide))
 
-(require 'github-browse-file)
+(require 'git-link)
 
-(defun github-save-url ()
-  "Add the github url for the current line or region to the kill ring. "
+(setq git-link-open-in-browser t)
+
+(defun facundo/git-link ()
   (interactive)
-  (let ((github-browse-file-visit-url nil))
-    (github-browse-file)))
+  (if current-prefix-arg
+      (let ((git-link-use-commit t)
+            (current-prefix-arg nil))
+        (call-interactively 'git-link))
+    (call-interactively 'git-link)))
+
+(global-set-key (kbd "C-x l") 'facundo/git-link)
 
 (provide 'facundo-git)
