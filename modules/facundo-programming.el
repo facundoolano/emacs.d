dc31c800379b2aef4796db303feaf002f502f8ef
Facundo Olano <facundo.olano@gmail.com>
2021-01-07 14:49:41 -0300

    more module reviews

--- modules/facundo-programming.el
+++ modules/facundo-programming.el
@@ -14,16 +14,6 @@
 
 ;;; TAKEN FROM prelude-programming.el
 
-;; in Emacs 24 programming major modes generally derive from a common
-;; mode named prog-mode; for others, we'll arrange for our mode
-;; defaults function to run prelude-prog-mode-hook directly.  To
-;; augment and/or counteract these defaults your own function
-;; to prelude-prog-mode-hook, using:
-;;
-;;     (add-hook 'prelude-prog-mode-hook 'my-prog-mode-defaults t)
-;;
-;; (the final optional t sets the *append* argument)
-
 ;; smart curly braces
 ;; (sp-pair "{ nil :post-handlers
 ;;          '(((lambda (&rest _ignored)
