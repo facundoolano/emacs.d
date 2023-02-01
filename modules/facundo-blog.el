8482fdd8f53e3bc4fee82337145a785a0e33b444
Facundo Olano <facundo.olano@gmail.com>
2023-02-01 15:41:25 -0300

    blog publish shortcut

--- modules/facundo-blog.el
+++ modules/facundo-blog.el
@@ -127,5 +127,8 @@ header."
 </div>
 </div>")
 
+(define-key org-blog-mode-map (kbd "s-e") 'org-blog-publish-file)
+(define-key org-blog-mode-map (kbd "s-E") 'org-blog-publish)
+
 (provide 'facundo-blog)
 ;;; facundo-blog.el ends here
