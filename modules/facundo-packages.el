780f70e388dddeb7c1d45afcbbf50ea719976d69
facundo <facundo.olano@gmail.com>
2025-04-06 23:25:19 -0300

    cleanup prelude-packages stuff

--- modules/facundo-packages.el
+++ modules/facundo-packages.el
@@ -1,47 +1,4 @@
-;;; prelude-packages.el --- Emacs Prelude: default package selection.
-;;
-;; Copyright © 2011-2016 Bozhidar Batsov
-;;
-;; Author: Bozhidar Batsov <bozhidar@batsov.com>
-;; URL: https://github.com/bbatsov/prelude
-;; Version: 1.0.0
-;; Keywords: convenience
-
-;; This file is not part of GNU Emacs.
-
-;;; Commentary:
-
-;; Takes care of the automatic installation of all the packages required by
-;; Emacs Prelude.
-
-;;; License:
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
-
-;;; Code:
 (require 'package)
-
-;; FIXME remove
-(defun prelude-require-package (package)
-  "Install PACKAGE unless already installed.")
-
-;; FIXME remove
-(defun prelude-require-packages (packages)
-  "Ensure PACKAGES are installed.
-Missing packages are installed automatically.")
 
 (use-package elmacro)
 (use-package persistent-scratch)
@@ -109,11 +66,4 @@ Missing packages are installed automatically.")
 
 (use-package dockerfile-mode :mode "Dockerfile\\'")
 
-
-;; FIXME rename to facundo packages
 (provide 'facundo-packages)
-;; Local Variables:
-;; byte-compile-warnings: (not cl-functions)
-;; End:
-
-;;; prelude-packages.el ends here
