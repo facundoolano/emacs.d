11f4f9e718be4eae3de55f83ed0b7337651b7237
facundo <facundo.olano@gmail.com>
2025-04-08 21:00:11 -0300

    rust eglot

--- modules/facundo-rust.el
+++ modules/facundo-rust.el
@@ -11,9 +11,10 @@
               ("s-f" . rustic-cargo-fmt)
               ("M-h" . rust-toggle-hints)))
 
+(setq rustic-lsp-client 'eglot)
+(setq-default eglot-workspace-configuration
+              '((:rust-analyzer . (:checkOnSave (:command "clippy")))))
 
-(setq rustic-format-on-save nil)
-(setq rustic-format-trigger nil)
 (setq rustic-compile-command "cargo clippy")
 (setq compilation-read-command nil)
 
