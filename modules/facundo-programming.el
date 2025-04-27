1e29f7a504a2fabd2a06bfba6f1b3d5c60b3e4ed
facundoolano <facundo.olano@gmail.com>
2025-04-27 20:07:25 -0300

    fix watcher ignore

--- modules/facundo-programming.el
+++ modules/facundo-programming.el
@@ -31,7 +31,7 @@
   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.node_modules\\'")
   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.venv\\'")
   (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.asdf\\'")
-  (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\/opt\\/homebrew\\'"))
+  (add-to-list 'lsp-file-watch-ignored-directories "/opt/homebrew"))
 
 (defun prelude-local-comment-auto-fill ()
   (set (make-local-variable 'comment-auto-fill-only-comments) t))
