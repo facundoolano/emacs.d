180b09d9721d8c31dad5f84439e42d80219f0cb4
facundo <facundo.olano@gmail.com>
2025-05-01 16:47:32 -0300

    gptel cleanup

--- modules/facundo-gpt.el
+++ modules/facundo-gpt.el
@@ -1,15 +1,3 @@
-(use-package gptel)
-
-(add-hook 'gptel-post-response-functions 'gptel-end-of-response)
-
-(gptel-make-anthropic "Claude"
-  :key gptel-api-key ;; get it from ~/.authinfo file
-  :stream t)
-
-(customize-set-variable 'gptel-backend (gptel-get-backend "Claude"))
-(customize-set-variable 'gptel-model 'claude-3-7-sonnet-20250219)
-(setq gptel-default-mode 'org-mode)
-
 (defun facundo/gptel-buffer ()
   "Switch or create a gptel session buffer, sending the marked region (if any)
 to the prompt. Similar to setting the `g` flag in the gptel menu."
@@ -18,19 +6,26 @@ to the prompt. Similar to setting the `g` flag in the gptel menu."
   (with-current-buffer (other-buffer nil t)))
 
 (defun facundo/gptel-send ()
-  "TODO"
+  "send the last prompt, making sure not in a middle of a sentence"
   (interactive)
   (when (and (not (use-region-p))
              (not (eolp)))
     (move-end-of-line 1))
   (gptel-send))
 
-(global-set-key (kbd "C-c g") 'facundo/gptel-buffer)
-(define-key gptel-mode-map (kbd "C-c RET") 'facundo/gptel-send)
-
-
-
-;; TODO
-;; - option to use claude alternatively
+(use-package gptel
+  :custom 
+  (gptel-log-level 'info)
+  (gptel-default-mode 'org-mode)
+  (gptel-backend (gptel-get-backend "Claude"))
+  (gptel-model 'claude-3-7-sonnet-20250219)
+  :bind
+  (("C-c g" . facundo/gptel-buffer)
+   :map gptel-mode-map ("C-c RET" . facundo/gptel-send))
+  :hook (gptel-post-response-functions . gptel-end-of-response)
+  :config
+  (gptel-make-anthropic "Claude"
+    :key gptel-api-key ;; get it from ~/.authinfo file
+    :stream t))
 
 (provide 'facundo-gpt)
