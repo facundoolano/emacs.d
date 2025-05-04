f3825fca855a32ff64bab2de550b99a62ab32af8
facundo <facundo.olano@gmail.com>
2025-05-04 16:24:11 -0300

    add customized filename

--- modules/facundo-gpt.el
+++ modules/facundo-gpt.el
@@ -1,10 +1,3 @@
-(defun facundo/gptel-buffer ()
-  "Switch or create a gptel session buffer, sending the marked region (if any)
-to the prompt. Similar to setting the `g` flag in the gptel menu."
-  (interactive)
-  (gptel (concat "**GPTel**") nil  nil t)
-  (with-current-buffer (other-buffer nil t)))
-
 (defcustom facundo/gptel-save-directory nil
   "Directory to save gptel buffers to when using `facundo/gptel-buffer`
  with prefix. 
@@ -13,24 +6,53 @@ Directory will be created if it doesn't exist."
   :type '(choice (const :tag "Default behavior" nil) directory)
   :group 'gptel)
 
-(defun facundo/gptel-buffer2 (&optional save-first)
+(defun facundo/gptel-generate-filename ()
+  "Generate a default filename for GPTel buffer based on content.
+Includes timestamp prefix and then the first prompt (after '***') if found."
+  (with-current-buffer "**GPTel**"
+    (let* ((timestamp (format-time-string "%Y%m%d"))
+           (first-prompt
+            (save-excursion
+              (goto-char (point-min))
+              (if (re-search-forward "^\\*\\*\\* \\(.*\\)" nil t)
+                  (match-string-no-properties 1)
+                (buffer-substring-no-properties
+                 (line-beginning-position)
+                 (min (+ (line-beginning-position) 40) (line-end-position))))))
+           (sanitized-prompt
+            (if (string-empty-p (string-trim first-prompt))
+                ""
+              (let ((name first-prompt))
+                (setq name (replace-regexp-in-string "[^a-zA-Z0-9_-]+" "-" name))
+                (setq name (replace-regexp-in-string "-+" "-" name))
+                (setq name (replace-regexp-in-string "^-\\|-$" "" name))
+                (setq name (substring name 0 (min (length name) 30)))
+                name))))
+      (if (string-empty-p sanitized-prompt)
+          (concat timestamp ".org")
+        (concat timestamp "-" sanitized-prompt ".org")))))
+
+(defun facundo/gptel-buffer (&optional arg)
   "Switch or create a gptel session buffer, sending the marked region (if any)
 to the prompt. Similar to setting the `g` flag in the gptel menu.
 
 With prefix argument (C-u), saves existing GPTel buffer first. If
 `facundo/gptel-save-directory` is set, saves to that directory."
   (interactive "P")
-  (when (and save-first
-             (get-buffer "**GPTel**"))
-    (with-current-buffer "**GPTel**"
-      (let ((default-directory
-             (if facundo/gptel-save-directory
-                 (progn
-                   (unless (file-exists-p facundo/gptel-save-directory)
-                     (make-directory facundo/gptel-save-directory t))
-                   facundo/gptel-save-directory)
-               default-directory)))
-        (call-interactively 'save-buffer))))
+  (when (and arg (get-buffer "**GPTel**"))
+    (let ((gptel-buffer (get-buffer "**GPTel**")))
+      ;; Switch to the GPTel buffer first
+      (switch-to-buffer-other-window gptel-buffer)
+      ;; Configure directory and save
+      (let* ((default-directory
+              (if facundo/gptel-save-directory
+                  (progn
+                    (unless (file-exists-p facundo/gptel-save-directory)
+                      (make-directory facundo/gptel-save-directory t))
+                    facundo/gptel-save-directory)
+                default-directory))
+             (suggested-name (facundo/gptel-generate-filename)))
+        (write-file (read-file-name "Save as: " default-directory nil nil suggested-name)))))
   (gptel "**GPTel**" nil nil t)
   (with-current-buffer (other-buffer nil t)))
 
@@ -55,7 +77,7 @@ With prefix argument (C-u), saves existing GPTel buffer first. If
   (gptel-model 'claude-3-7-sonnet-20250219)
   (facundo/gptel-save-directory "~/dev/gptel/")
   :bind
-  (("C-c g" . facundo/gptel-buffer2)
+  (("C-c g" . facundo/gptel-buffer)
    :map gptel-mode-map ("C-c RET" . facundo/gptel-send))
   :hook (gptel-post-response-functions . gptel-end-of-response))
 
